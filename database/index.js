4e4ecc39c85275b951e330c2187079ecb8ef1699
RamKhalsa <khalsa.ramdas@gmail.com>
2019-03-29 11:51:15 -0700

    Implement minor bugfix and seed command

--- database/index.js
+++ database/index.js
@@ -5,6 +5,7 @@ const pw = require('./config/sequelize.config');
 const db = new Sequelize('bookings', 'root', pw.rootPW, {
   host: 'localhost',
   dialect: 'mysql',
+  logging: false,
 });
 
 module.exports = db;
