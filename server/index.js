7b01ea2f79b544605870c136cfcaaade0d56cfc6
RamKhalsa <khalsa.ramdas@gmail.com>
2019-03-30 10:26:27 -0700

    revise API endpoint

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const sequelize = require('../database/index');
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(morgan('dev'));
 
-app.get('/bookings/:accommodationid', (req, res) => {
+app.get('/bookings/:accommodationid/reserve', (req, res) => {
   sequelize.query(`SELECT * FROM reservations INNER JOIN accommodation INNER JOIN guests 
   WHERE reservations.accommodation_id = accommodation.id AND guests.id = reservations.guest_id AND accommodation.id = ${req.params.accommodationid};`)
     .then(reservations => (res.send(JSON.stringify(reservations))));
