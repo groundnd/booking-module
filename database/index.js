041147225776451c326aca1f613db80b75044b43
RamKhalsa <khalsa.ramdas@gmail.com>
2019-03-30 10:07:45 -0700

    Add process.env variables

--- database/index.js
+++ database/index.js
@@ -1,9 +1,11 @@
 const Sequelize = require('sequelize');
-const pw = require('./config/sequelize.config') || null;
+const pw = process.env.MYSQL_ROOT_PASSWORD || require('./config/sequelize.config').rootPW;
+
+const databaseName = process.env.MYSQL_DATABASE || 'bookings';
 
 
-const db = new Sequelize('bookings', 'root', pw.rootPW, {
-  host: 'localhost',
+const db = new Sequelize(databaseName, 'root', pw, {
+  host: process.env.MYSQL_URL || 'localhost',
   dialect: 'mysql',
   logging: false,
 });
