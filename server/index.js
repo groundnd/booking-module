82fbe5cb2c65057f26acdddcbf0dc3cad0f6198c
M-Shehu <muhammadshehu@Shehus-MacBook-Pro.local>
2019-05-01 23:14:21 -0700

    Update to remove redis cache

--- server/index.js
+++ server/index.js
@@ -31,40 +31,24 @@ app.get('/bookings/:accommodationid/:startDate&:endDate', (req, res) => {
   const start = new Date(req.params.startDate);
   const current = new Date(req.params.endDate);
   
-  return getRedisAsync(req.params.accommodationid)
-  .then(async (result) => {
-    if (result) {
-      res.send(result);
-    } else {
-      const availability = await postgres.getReservation(req.params.accommodationid, dateFormatter(start), dateFormatter(current));
-      res.send(JSON.stringify({ availability }));
-      setRedisAsync(`${req.params.accommodationid}-reservation`, 3600, JSON.stringify({ availability }))
-    }
-  })
+  const availability = await postgres.getReservation(req.params.accommodationid, dateFormatter(start), dateFormatter(current));
+  res.send(JSON.stringify({ availability }));
   
 });
 
 app.get('/bookings/:accommodationid', (req, res) => {
-
-  return getRedisAsync(req.params.accommodationid)
-  .then(async (result) => {
-    if (result) {
-      res.send(result);
-    } else {
-      const accommodation = await postgres.getAccommodation(req.params.accommodationid);
-      const now = new Date();
-      let current;
-      if (now.getMonth() === 11) {
-        current = new Date(now.getFullYear() + 1, 0, 1);
-      } else {
-        current = new Date(`${now.getMonth() + 2} 1, ${now.getFullYear()}`);
-      }
-      const availability = await postgres.getReservation(req.params.accommodationid, dateFormatter(now), dateFormatter(current));
-      // console.log(availability);
-      !availability ? res.send(JSON.stringify({ accommodation, availability: {} })) : res.send(JSON.stringify({ accommodation, availability }));
-      setRedisAsync(`${req.params.accommodationid}-accommodation`, 3600, JSON.stringify({ accommodation, availability }))
-    }
-  })
+  const accommodation = await postgres.getAccommodation(req.params.accommodationid);
+  const now = new Date();
+  let current;
+  if (now.getMonth() === 11) {
+    current = new Date(now.getFullYear() + 1, 0, 1);
+  } else {
+    current = new Date(`${now.getMonth() + 2} 1, ${now.getFullYear()}`);
+  }
+  const availability = await postgres.getReservation(req.params.accommodationid, dateFormatter(now), dateFormatter(current));
+  // console.log(availability);
+  !availability ? res.send(JSON.stringify({ accommodation, availability: {} })) : res.send(JSON.stringify({ accommodation, availability }));
+   
 });
 
 // eslint-disable-next-line no-console
